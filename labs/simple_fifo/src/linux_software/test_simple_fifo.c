d1ed1bf12c83002bf5573103abab29c30d0aa65c
Hunter Mills <hmills@USSEV1-JYNPJR2.jade.jacobs.com>
2023-11-14 16:27:02 -0500

    Fixed syntax

--- labs/simple_fifo/src/linux_software/test_simple_fifo.c
+++ labs/simple_fifo/src/linux_software/test_simple_fifo.c
@@ -21,19 +21,6 @@ volatile unsigned int * get_a_pointer(unsigned int phys_addr)
 	return (fifo_base);
 }
 
-
-
-// Function to assert and deassert Reset
-void reset(volatile unsigned int *ptrToFifo, int set){
-    // Toggle reset
-    if(set == 1){
-        *(ptrToFifo+SIMPLE_FIFO_RESET_OFFSET) = 1;
-    }
-    else {
-        *(ptrToFifo+SIMPLE_FIFO_RESET_OFFSET) = 0;
-    }
-}
-
 // Function to check if Simple FIFO data is correct
 void check_fifo(int * fifo_data, int num_reads){
     // Fill test array
@@ -45,39 +32,34 @@ void check_fifo(int * fifo_data, int num_reads){
     // Compare Test array and Simple FIFO data
     int num_errors = 0;
     for(int i = 0; i < num_reads; i++){
-        if(test_values[i] != fifo_data[i]){
+        if(test_values[i] != *(fifo_data+i)){
             printf("ERROR at sample %d", i);
             num_errors++;
         }
     }
-    printf("NUMBER OF ERRORS: %d", num_errors);
+    printf("\tNUMBER OF ERRORS: %d", num_errors);
 }
 
 // Function to read a specified number from the Simple FIFO
-void read_fifo(volatile unsigned int *ptrToFifo, int num_reads) {
-    int fifo_values[num_reads];
+void read_fifo(volatile unsigned int *ptrToFifo, int* fifo_data, int num_reads) {
     for(int i=0; i<num_reads; i++){
         //Check to see if fifo is empty
         if(*(ptrToFifo+SIMPLE_FIFO_COUNTER_OFFSET) > 0){
-            fifo_values[i] = *(ptrToFifo+SIMPLE_FIFO_DATA_OFFSET);
+            *(fifo_data + i) = *(ptrToFifo+SIMPLE_FIFO_DATA_OFFSET);
         }
     }
-    check_fifo(&fifo_values, num_reads);
+    check_fifo(fifo_data, num_reads);
 }
 
-// void print_benchmark(int time){
-//     // Find the benchmark metrics
-//     printf("Elapsed time: %f\n",stop_time-start_time);
-//     float throughput=0; 
-//     // please insert your code here for calculate the actual throughput in Mbytes/second
-//     // how much data was transferred? How long did it take?
-//     unsigned int bytes_transferred = 2048*4;					// # of Reads * 4bytes (32bits)
-//     float clk_cycles = stop_time - start_time;					// Get number of clk cycles
-//     float time_spent = clk_cycles / 125000000;					// clk cycles / clk speed
-//     throughput = bytes_transferred/time_spent/1000000;			// bytes / time / 1MB
-//     printf("You transferred %u bytes of data in %f seconds\n",bytes_transferred,time_spent);
-//     printf("Measured Transfer throughput = %f Mbytes/sec\n",throughput);
-// }
+void print_benchmark(float time, int num_reads){
+    // Find the benchmark metrics
+    printf("Elapsed time: %f\n",time);
+    float throughput=0; 
+    unsigned int bytes_transferred = 4*num_reads;				// # of Reads * 4bytes (32bits)
+    throughput = bytes_transferred/time/1000000;				// bytes / time / 1MB
+    printf("You transferred %u bytes of data in %f seconds\n",bytes_transferred,time);
+    printf("Measured Transfer throughput = %f Mbytes/sec\n",throughput);
+}
 
 int main(int argc, char* argv[]) {
     // Check only one command line arg
@@ -85,28 +67,29 @@ int main(int argc, char* argv[]) {
         printf("ERROR: Number of command line args must be 1\n\r");
         return -1;
     }
+    int num_reads = *argv[0];
 
     // Get Pointer to Simple FIFO
     volatile unsigned int *simple_fifo = get_a_pointer(SIMPLE_FIFO_ADDRESS);	
 
     // Menu
-    printf("\r\n\r\n\r\nMilestone 1 Hunter Mills - Simple FIFO\n\r");
-    printf("Number of reads: %d", *argv[0]);
+    printf("\r\n\r\n\r\n");
+    printf("Milestone 1 Hunter Mills - Simple FIFO\n\r");
+    printf("Number of reads: %d\n\r", num_reads);
     
     // Reset PL
-    reset(simple_fifo, 0);
+    *(simple_fifo+SIMPLE_FIFO_RESET_OFFSET) = 0;
+    *(simple_fifo+SIMPLE_FIFO_RESET_OFFSET) = 1;
     
     // Read from Simple FIFO
-    int * fifo_data;
-    //printf("Reading ... \n\r");
+    int fifo_data[num_reads];
+    printf("Reading ... \n\r");
     clock_t start = clock();    // Find start time
-    read_fifo(simple_fifo, *argv[0]);
-    //clock_t end = clock();       // Find end time
+    read_fifo(simple_fifo, fifo_data, num_reads);
+    clock_t end = clock();       // Find end time
     printf("DONE\n\r");
 
-
-    
     // Print Metrics
-    //print_benchmark(int(end-start));
+    print_benchmark((float)(end-start)/CLOCKS_PER_SEC, num_reads);
     return 0;
 }
